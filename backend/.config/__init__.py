03befe6aadaea36db92c3f666472a4a2c37af874
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2026-06-04 22:19:04 +0530

    Delete backend/.config directory

--- backend/.config/__init__.py
+++
@@ -1,4 +0,0 @@
-from .settings import Settings, get_settings, settings
-from .logging_config import configure_logging, get_logger
-
-__all__ = ["Settings", "get_settings", "settings", "configure_logging", "get_logger"]

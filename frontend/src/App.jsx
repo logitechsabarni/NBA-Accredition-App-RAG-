4be58c01be2073421e89ce4fc75913e3a523e1f6
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2026-06-05 21:48:11 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;

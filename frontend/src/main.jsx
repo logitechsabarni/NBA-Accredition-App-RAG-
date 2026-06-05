c88f0a9fd7258b381a0261d097247fc2acdf52e9
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2026-06-05 22:40:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./App";
-
-import { store } from "./store";
+import store from "./store";
 
 import "./index.css";
 

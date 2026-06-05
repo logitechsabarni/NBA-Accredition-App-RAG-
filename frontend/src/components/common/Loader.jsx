6ed2e3c6f7b36d5227140b2e24ea0c7cb8b677bd
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2026-06-05 22:26:39 +0530

    Create Loader.jsx

---
+++ frontend/src/components/common/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-full w-full">
+      <div className="h-12 w-12 rounded-full border-4 border-blue-500 border-t-transparent animate-spin"></div>
+    </div>
+  );
+}
